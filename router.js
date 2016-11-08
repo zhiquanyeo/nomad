c426fd51351547f6084583a0456c43388ed044f3
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2016-11-07 23:25:30 -0500

    Add connect/disconnect methods to router

--- router.js
+++ router.js
@@ -17,8 +17,31 @@ class NomadRouter {
 		delete this.d_nodes[clientId];
 	}
 
-	setConnection(clientA, clientB) {
+	connect(clientA, clientB) {
+		if (!this.d_nodes[clientA]) {
+			console.log('Node ' + clientA + ' is not registered');
+			return;
+		}
+		if (!this.d_nodes[clientB]) {
+			console.log('Node ' + clientB + ' is not registered');
+			return;
+		}
 
+		this.d_nodes[clientA].endpoint = clientB;
+		this.d_nodes[clientB].endpoint = clientA;
+	}
+
+	disconnect(clientId) {
+		if (this.d_nodes[clientId]) {
+			if (this.d_nodes[clientId].endpoint) {
+				var ep = this.d_nodes[clientId].endpoint;
+				this.d_nodes[ep].endpoint = undefined;
+			}
+			this.d_nodes[clientId].endpoint = undefined;
+		}
+		else {
+			console.log('Node ' + clientId + ' is not registered');
+		}
 	}
 }
 
